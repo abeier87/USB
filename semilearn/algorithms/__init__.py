086831f73a18bc5380d9cae7f340b536c17362b6
SQ <qianshao@zju.edu.cn>
2024-11-01 16:18:42 +0000

    refactor(algorithms): 清理 get_algorithm 函数中的注释和多余空行

--- semilearn/algorithms/__init__.py
+++ semilearn/algorithms/__init__.py
@@ -6,7 +6,7 @@ name2alg = ALGORITHMS
 
 def get_algorithm(args, net_builder, tb_log, logger):
     if args.algorithm in ALGORITHMS:
-        alg = ALGORITHMS[args.algorithm]( # name2alg[args.algorithm](
+        alg = ALGORITHMS[args.algorithm](
             args=args,
             net_builder=net_builder,
             tb_log=tb_log,
@@ -15,6 +15,3 @@ def get_algorithm(args, net_builder, tb_log, logger):
         return alg
     else:
         raise KeyError(f'Unknown algorithm: {str(args.algorithm)}')
-
-
-
--- semilearn/core/__init__.py
+++ semilearn/core/__init__.py
@@ -5,4 +5,4 @@
 from .algorithmbase import AlgorithmBase, ImbAlgorithmBase
 from .utils.registry import import_all_modules_for_register
 
-import_all_modules_for_register()
+import_all_modules_for_register() # import all algs in ALL_MODULES
