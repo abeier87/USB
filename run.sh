356923aafb0b6f51c59b3ed882e0815bd965240d
SQ <12221112@zju.edu.cn>
2024-11-28 07:34:46 +0000

    feat(cleanup): 删除run.py、run.sh和test.py文件，移除不再使用的训练和测试代码

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 设置可见的CUDA设备为0
-export CUDA_VISIBLE_DEVICES=1
-# 执行Python训练脚本及相关参数配置
-python train.py --c config/classic_cv_imb/fixmatch_debiaspl/fixmatch_debiaspl_cifar10_lb1500_100_ulb3000_100_1.yaml
-
-CUDA_VISIBLE_DEVICES=2 python train.py --c config/classic_cv_imb/fixmatch_debiaspl/fixmatch_debiaspl_cifar10_lb1500_100_ulb3000_100_1.yaml
